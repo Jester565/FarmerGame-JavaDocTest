6569ab0ba1945ed033f4bd0b91889e24f5b93edf
Jester565 <ajcraig543@gmail.com>
2016-10-05 13:46:10 -0700

    DAMAGE static, naming conventions, remove platYOff
    Made DAMAGE static, improved description of draw(float, float) method.
    Changed naming conventions so that the first letter of a data member is lower case.
    Removed the platYOff variable because the constructor was not initializing it and it remained at 0.

--- Spike.java
+++ Spike.java
@@ -6,46 +6,41 @@ package edu.cpp.cs.cs141.prog_assgmnt_1;
 
 public class Spike {
 	/**
+	 * This field represents the damage the player will take upon contact with the spike. This field cannot be modified.
+	 */
+	private static final int DAMAGE;
+	/**
 	 * This field represents the horizontal position of the Spike. Initially {@code 0}, 
 	 * this field will be changed upon creation of the Spike. After its first reassignment, 
 	 * the field will not be reassigned.
 	 */
-	private float OffPlatX;
-	/**
-	 * This field represents the vertical position of the Spike. Initially {@code 0}, 
-	 * this field will be changed upon creation of the Spike. After its first reassignment, 
-	 * the field will not be reassigned.
-	 */
-	private float OffPlatY;
+	private float offPlatX;
 	/**
 	 * This field represents the height of the spike. Initially {@code 0}, this field will be changed upon creation of the Spike.
 	 * After its first reassignment, the field will not be reassigned.
 	 */
-	private int SpikeHeight;
+	private int spikeHeight;
 	/**
 	 * This field represents the width of the spike. Initially {@code 0}, the field will be changed upon creation of the Spike.
 	 * After its first reassignment, the field will not be reassigned.
 	 */
-	private int SpikeWidth;
+	private int spikeWidth;
 	/**
-	 * This field represents the damage the player will take upon contact with the spike. This field cannot be modified.
+	 * This is the constructor for the Spike. Will have {@link offPlatX}. {@link spikeHeight}, and {@link spikeWidth}
 	 */
-	private final int DAMAGE;
-	/**
-	 * This is the constructor for the Spike. Will have {@link OffPlatX}. {@link SpikeHeight}, and {@link SpikeWidth}
-	 */
-	public Spike(float OffPlatX, int SpikeHeight, int SpikeWidth){
+	public Spike(float offPlatX, int spikeHeight, int spikeWidth){
 		
 	}
 	/**
-	 * Draws the Spike at platformX and platformY location along with its height and width.
+	 * Draws the Spike at platformX {@code +} {@link #offPlatX} and platformY location along with its height, represented by
+	 {@link #spikeHeight}, and width, represented by {@link #spikeWidth}.
 	 */
 	public void draw(float platformX, float platformY){
 		
 	}
 	/**
-	 * This method calculates damage against the Farmer
-	 * @param Farmer health value is reduced by {@link #Damage}
+	 * This method calculates damage against the {@link Farmer}
+	 * @param farmer calls {@link Farmer#takeDamage(int) with {@link #Damage} as the damage parameter.
 	 */
 	public void checkDamage(Farmer farmer){
 		
