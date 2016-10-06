9b733901ad8cc160e3eb42d84c3160c9e74823f1
Jester565 <ajcraig543@gmail.com>
2016-10-05 19:34:10 -0700

    speedMultiplier instead of speedIncrease, fixed naming conventions

--- Item.java
+++ Item.java
@@ -12,7 +12,7 @@ public class Item {
 	 * This is a special number assigned to each item to identify it. Each item has a different
 	 * number.
 	 */
-	private int ID;
+	private int id;
 	
 	/**
 	 * The description is an explanation of what the item does; weather it can heal the {@link Farmer}
@@ -26,9 +26,9 @@ public class Item {
 	private int healthRestored;
 	
 	/**
-	 * This field stores weather the item will speed the {@link Farmer} up and if so by how much.
+	 * This field stores how much the item will speed the {@link Farmer} up and if so by how much.
 	 */
-	private int speedGained;
+	private float speedMultiplier;
 	
 	/**
 	 * This field allows the item class to access {@link Farmer} class so that the
@@ -39,8 +39,12 @@ public class Item {
 	/**
 	 * This is the default contructor for the class {@link Item}. It will contain {@link Farmer} as
 	 * well as default conditions for the {@link #speedGained}, {@link #healthRestored} and {@link #ID}.
+	 * @param farmer The farmer that the item effects will apply to.
+	 * @param speedMultiplier How much the item will increase the speed of the player by.  Set to {@code 1} for no effect.
+	 * @param healthRestored How much the item will increase the players health by.  Set to {@code 0} for no effect.
+	 * @param id The id number of the Item.
 	 */
-	public Item( Farmer Farmer, int speedGained, int healthRestored, int ID ){
+	public Item( Farmer farmer, float speedMultiplier, int healthRestored, int id ){
 		
 	}
 	
